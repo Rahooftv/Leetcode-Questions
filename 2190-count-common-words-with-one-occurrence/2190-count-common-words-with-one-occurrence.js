07b8a14fd1320b6fbda3aa8ece3b3e7682859f34
Rahooftv <rahooftv5885@gmail.com>
2025-04-15 15:23:56 +0530

    Time: 75 ms (5.49%), Space: 58.7 MB (53.11%) - LeetHub

---
+++ 2190-count-common-words-with-one-occurrence/2190-count-common-words-with-one-occurrence.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} words1
+ * @param {string[]} words2
+ * @return {number}
+ */
+var countWords = function(words1, words2) {
+
+   words1 = words1.filter((word,ind,arr)=>arr.indexOf(word)==arr.lastIndexOf(word))
+   words2 =words2.filter((word,ind,arr)=>arr.indexOf(word)==arr.lastIndexOf(word))
+
+
+   let words = [...words1,...words2]
+   let filterword = words.filter((word,ind,arr)=>arr.indexOf(word)!==arr.lastIndexOf(word))
+   let duplicate = [...new Set(filterword)]
+    return duplicate.length
+};
