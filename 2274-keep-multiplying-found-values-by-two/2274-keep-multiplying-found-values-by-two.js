86ac9ce8a7452e4d1a6c9758f3b3f65936140092
Rahooftv <rahooftv5885@gmail.com>
2025-07-16 12:53:42 +0530

    Time: 1 ms (52.8%), Space: 53.5 MB (87.38%) - LeetHub

---
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} original
+ * @return {number}
+ */
+var findFinalValue = function(nums, original) {
+
+    for(i=0;i<nums.length;i++){
+        if(nums[i]===original){
+            original=original*2
+            i=-1
+
+
+        }
+    }
+    return original
+    
+};
