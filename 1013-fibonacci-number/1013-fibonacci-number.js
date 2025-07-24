34cf908d3badc4a84dc9037bbea0d8142a204b3f
Rahooftv <rahooftv5885@gmail.com>
2025-07-24 12:38:20 +0530

    Time: 35 ms (93.97%), Space: 53.3 MB (69.52%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.js
+++ 1013-fibonacci-number/1013-fibonacci-number.js
@@ -3,8 +3,15 @@
  * @return {number}
  */
 var fib = function(n) {
-    
-    if (n <= 1) return n;
-    return fib(n - 1) + fib(n - 2)
+    if(n<=1) return n
+   let a =0 
+   let b =1
+   for(i=2;i<=n;i++){
+    let temp = a+b
+    a = b
+    b = temp 
+
+   }
+   return b
     
 };
